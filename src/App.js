e52bc5e413ff5c877f544698078854aeb9de3a24
duane-faria <duane.faria@etec.sp.gov.br>
2020-09-01 19:34:17 -0300

    Usando formulário com os recursos do hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
 import React from 'react';
 
 function App() {
-  return <div />;
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+
+  const [form, setForm] = React.useState({
+    name: '',
+    email: '',
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
+  function handleChange({ target }) {
+    const { id, value } = target;
+    setForm({ ...form, [id]: value });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Nome</label>
+      <input
+        value={form.name}
+        type="text"
+        id="name"
+        name="name"
+        onChange={handleChange}
+      />
+
+      <label htmlFor="email">E-mail</label>
+      <input
+        value={form.email}
+        type="email"
+        id="email"
+        name="email"
+        onChange={handleChange}
+      />
+
+      <button>Enviar</button>
+    </form>
+  );
 }
 
 export default App;
